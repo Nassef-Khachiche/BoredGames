136db8358606a3e544e6b62319e97906dc62f95e
Nassef Khachiche <nassefkhachiche1@gmail.com>
2023-03-06 00:26:51 +0100

    Updated - Dashboard and page redirects

--- public/js/active-link.js
+++ public/js/active-link.js
@@ -3,6 +3,6 @@ const navlink = document.querySelectorAll('.nav-link')
 function activeLink() {
     navlink.forEach((item) => item.classList.remove('active'))
     this.classList.add('active')
-}
+}   
 
 navlink.forEach((item) => item.addEventListener('click', activeLink))
